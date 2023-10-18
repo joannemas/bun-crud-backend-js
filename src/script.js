8b10e75682411f267a55e5c1c1f899b370ebd7c3
Joanne <90553540+joannemas@users.noreply.github.com>
2023-10-19 00:11:43 +0200

    ✨ Update pokemon

--- src/script.js
+++ src/script.js
@@ -23,6 +23,7 @@ window.addEventListener("DOMContentLoaded", function () {
                 const listItem = document.createElement("li");
                 listItem.innerHTML = `
                     ID: ${pokemon._id} <br> Name: ${pokemon.name} <br> Type: ${pokemon.type}
+                    <button class="updateButton" data-id="${pokemon._id}">Mettre à jour</button>
                     <button class="deleteButton" data-id="${pokemon._id}">Supprimer</button>
                 `;
                 pokemonList.appendChild(listItem);
@@ -36,15 +37,26 @@ window.addEventListener("DOMContentLoaded", function () {
     // Appeler la fonction pour afficher la liste des Pokémons au chargement de la page
     displayPokemons();
 
+    // Fonction du bouton pour supprimer ou mettre à jour un Pokémon
     document.getElementById("pokemonList").addEventListener("click", (event) => {
-        if (event.target.classList.contains("deleteButton")) {
+        if (event.target.classList.contains("updateButton")) {
+            const pokemonId = event.target.getAttribute("data-id");
+            console.log(`Mise à jour du Pokémon avec l'identifiant ${pokemonId}`);
+            
+            const updatedData = prompt("Modifier le nom et le type du Pokémon (séparés par une virgule)");
+            if (updatedData) {
+                const [name, type] = updatedData.split(",");
+                updatePokemon(pokemonId, name, type);
+            }
+        } else if (event.target.classList.contains("deleteButton")) {
             const pokemonId = event.target.getAttribute("data-id");
             console.log(`Suppression du Pokémon avec l'identifiant ${pokemonId}`);
             if (confirm("Voulez-vous vraiment supprimer ce Pokémon ?")) {
                 deletePokemon(pokemonId);
             }
         }
-    });       
+    });
+    
 
     // Fonction pour ajouter un nouveau Pokémon
     function addPokemon() {
@@ -102,6 +114,30 @@ window.addEventListener("DOMContentLoaded", function () {
         });
     }    
         
+
+    // Fonction pour mettre à jour un Pokémon
+    function updatePokemon(pokemonId, name, type) {
+        fetch(`/pokemons/${pokemonId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name, type }),
+        })
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw Error("Erreur lors de la mise à jour du Pokémon");
+            }
+        })
+        .then(() => {
+            displayPokemons(); // Mettez à jour la liste des Pokémons après la mise à jour
+        })
+        .catch((error) => {
+            console.error(error.message);
+        });
+    }
 
     // Associer des événements aux boutons
     document.getElementById("addPokemonButton").addEventListener("click", addPokemon);
