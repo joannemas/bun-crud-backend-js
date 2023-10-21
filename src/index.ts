1d117ba8a15987806d3b353fa0170e7f919d9d66
Joanne <90553540+joannemas@users.noreply.github.com>
2023-10-21 15:40:10 +0200

    :art: Change structure

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,10 @@ app.use(cors());
 app.use(html());
 app.use(docsSetup);
 
-app.get('/', () => Bun.file('src/index.html').text());
+app.get('/', () => Bun.file('src/public/index.html').text());
 app.get('/script.js', () => Bun.file('src/script.js').text());
 
-app.get('/log', () => Bun.file('src/login.html').text());
+app.get('/log', () => Bun.file('src/public/login.html').text());
 app.get('/log.js', () => Bun.file('src/log.js').text());
 
 PokemonController(app);
